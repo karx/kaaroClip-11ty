7e2ac66967dc87d2d954c123f8f401a646c1e248
Kartik <kartik.arora1214@gmail.com>
2021-09-02 11:53:05 +0530

    base working

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,7 @@
 require('dotenv').config();
 
-module.exports = function(eleventyConfig) {
-  eleventyConfig.addPassthroughCopy('css')
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy('css');
+  eleventyConfig.addPassthroughCopy('sound');
+
 }
