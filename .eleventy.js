fdc2f3f7a60596a17a1105fd2c865ef210db197b
Kartik <kartik.arora1214@gmail.com>
2021-09-06 17:27:55 +0530

    UI changes as per notes from Ankit

--- .eleventy.js
+++ .eleventy.js
@@ -3,5 +3,6 @@ require('dotenv').config();
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy('css');
   eleventyConfig.addPassthroughCopy('sound');
+  eleventyConfig.addPassthroughCopy('images');
 
 }
